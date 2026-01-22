ab3ae5ba50f2eb90593312d943ae82d069da1716
Matjaz Prtenjak <matjaz@matjazev.net>
2026-01-22 18:41:11 +0100

    Urejeni DATA unit testi

--- 2025/Programiranje1/TestiranjeS_QT/tst_primer1.cpp
+++ 2025/Programiranje1/TestiranjeS_QT/tst_primer1.cpp
@@ -15,6 +15,10 @@ public:
 private slots:
     void test_case1();
 
+    void parseTest_data();
+    void parseTest();
+
+    /*
     void WhenInputIsCorrect_then_ExpectCorrectResult();
     void WhenInputIsCorrect_then_ExpectCorrectResult1();
     void WhenInputISIncorrect_the_ExcpectFail();
@@ -22,14 +26,99 @@ private slots:
     void WhenInputHasSpaces_the_ExcpectNoSpaces();
     void WhenNumberIsNegative_then_ExpectCorrectResult();
     void WhenNumberIsNotInteger_then_ExpectException();
+    */
 };
 
 primer1::primer1() {}
 
 primer1::~primer1() {}
 
+void primer1::parseTest_data()
+{
+//    QString	bool
+//        dataLine	dataSuccess
+
+    QTest::addColumn<QString>("dataLine");
+    QTest::addColumn<bool>("expectThrow");
+    QTest::addColumn<bool>("dataSuccess");
+    QTest::addColumn<QString>("dataName");
+    QTest::addColumn<int>("dataPoints");
+
+    QTest::newRow("WhenInputIsCorrect_then_ExpectCorrectResult")
+        << QString("jan;2423")
+        << false
+        << true
+        << QString("jan")
+        << 2423;
+
+    QTest::newRow("WhenPointsAre0_then_ExpectCorrectResult")
+        << QString("feb;0")
+        << false
+        << true
+        << QString("feb")
+        << 0;
+
+    QTest::newRow("WhenInputDoesNotHaveComma_the_ExcpectFail")
+        << QString("xxxxxxx")
+        << false
+        << false;
+
+    QTest::newRow("WhenInputIsNotInteger_the_ExcpectException")
+        << QString("xxxx;xxx")
+        << true;
+
+    QTest::newRow("WhenInputHasSpaces_the_ExcpectNoSpaces")
+        << QString("        Matilda     ;   12   ")
+        << false
+        << true
+        << QString("Matilda")
+        << 12;
+
+    QTest::newRow("WhenNumberIsNegative_then_ExpectCorrectResult")
+        << QString("xxx;-13")
+        << false
+        << true
+        << QString("xxx")
+        << -13;
+
+    QTest::newRow("WhenNumberIsNotInteger_then_ExpectException")
+        << QString("xxx;13.4")
+        << true;
+}
+
+void primer1::parseTest()
+{
+    // ARR
+    QFETCH(QString, dataLine);
+    QFETCH(bool, expectThrow);
+
+    if (expectThrow) {
+        Record rec;
+        QVERIFY_THROWS_EXCEPTION(std::exception, bool rezultat = parseRecord(dataLine, rec));
+        return;
+    }
+
+    QFETCH(bool, dataSuccess);
+
+    // ACT
+    Record rec;
+    bool rezultat = parseRecord(dataLine, rec);
+
+    // ASS
+    QCOMPARE(rezultat, dataSuccess);
+
+    if (dataSuccess) {
+        QFETCH(QString, dataName);
+        QFETCH(int, dataPoints);
+
+        QCOMPARE(rec.name, dataName);
+        QCOMPARE(rec.points, dataPoints);
+    }
+}
+
 void primer1::test_case1() {}
 
+/*
 void primer1::WhenInputIsCorrect_then_ExpectCorrectResult() {
     // ARR
     QString test = "jan;2423";
@@ -120,6 +209,7 @@ void primer1::WhenNumberIsNotInteger_then_ExpectException() {
     QVERIFY_THROWS_EXCEPTION(std::exception, bool rezultat = parseRecord(test, vhod));
 
 }
+*/
 
 QTEST_APPLESS_MAIN(primer1)
 
