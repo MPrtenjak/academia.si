a07aa8ebdf8ce2579796714563842c77af749419
Matjaz Prtenjak <matjaz@matjazev.net>
2026-02-27 10:10:15 +0100

    Rešitve nalog za prvo srečanje

--- 2025/Programiranje2/napredno.cpp
+++ 2025/Programiranje2/napredno.cpp
@@ -234,3 +234,139 @@ int main()
 
 #endif
 
+#ifdef STATIC_FUNCTIONS_10
+
+class Math {
+public:
+    static int max(int a, int b) {
+        return (a > b) ? a : b;
+    }
+
+    static int abs(int x) {
+        return (x < 0) ? -x : x;
+    }
+};
+
+int main() {
+    std::cout << Math::max(3, 10) << "\n";   // 10
+    std::cout << Math::abs(-7) << "\n";      // 7
+}
+
+#endif
+
+#ifdef STATIC_FUNCTIONS_20
+
+class Counter {
+private:
+    static int created;   // skupno za vse objekte
+
+public:
+    Counter() { created++; }
+
+    static int howManyCreated() {  // vezano na razred
+        return created;
+    }
+
+    // zmanjšaj števec ob uničenju objekta
+	~Counter() { created--; }  
+};
+
+int Counter::created = 0; // definicija statičnega člana
+
+int main() {
+    Counter a, b, c;
+    std::cout << Counter::howManyCreated() << "\n"; // 3
+
+    Counter arr[13];
+    std::cout << Counter::howManyCreated() << "\n"; // 16
+
+	Counter* p = new Counter();
+    std::cout << Counter::howManyCreated() << "\n"; // 17
+	delete p;
+
+    std::cout << Counter::howManyCreated() << "\n"; // 16
+
+	return 0;
+}
+
+#endif
+
+#ifdef NADGRAJEVANJE_OPERATORJEV_10
+
+struct Vec2 {
+    int x, y;
+
+    Vec2(int x_ = 0, int y_ = 0) : x(x_), y(y_) {}
+
+    Vec2 operator+(const Vec2& o) const {
+        return Vec2(x + o.x, y + o.y);
+    }
+
+    Vec2 operator+(const int offset) const {
+        return Vec2(x + offset, y + offset);
+    }
+
+    bool operator==(const Vec2& o) const {
+        return x == o.x && y == o.y;
+    }
+};
+
+int main() {
+    Vec2 a(2, 3), b(10, -1);
+    Vec2 c = a + b;
+    std::cout << c.x << "," << c.y << "\n";
+    std::cout << (c == Vec2(12, 2)) << "\n";
+
+	Vec2 d = a + 5;
+    std::cout << d.x << "," << d.y << "\n";
+    std::cout << (d == Vec2(7, 8)) << "\n";
+	return 0;
+}
+
+#endif
+
+#ifdef NADGRAJEVANJE_OPERATORJEV_20
+
+struct Log {
+    Log& operator<<(const char* s) {
+        std::cout << "[LOG] " << s << endl;
+        return *this;
+    }
+    Log& operator<<(int x) {
+        std::cout << "[LOG] " << x << endl;
+        return *this;
+    }
+};
+
+int main() {
+    Log log;
+    log << "Score = " << 42 << "Age = "  << 13;
+}
+
+#endif
+
+#ifdef NADGRAJEVANJE_OPERATORJEV_30
+
+struct Tiny3 {
+    int a[3];
+
+    int& operator[](int i) {
+        if (i < 0 || i >= 3) throw std::out_of_range("index");
+        return a[i];
+    }
+    const int& operator[](int i) const {
+        if (i < 0 || i >= 3) throw std::out_of_range("index");
+        return a[i];
+    }
+};
+
+int main() {
+    Tiny3 t;
+    t[0] = 7;
+    t[1] = 8;
+    t[2] = 9;
+
+    std::cout << t[1] << "\n"; // 8
+}
+
+#endif
